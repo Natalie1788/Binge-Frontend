e72952290fbf18a3002740b415bc870cab9c8e49
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-05-02 15:13:24 +0200

    Resolved signup/sign in functionality

--- Binge/src/pages/Landing.jsx
+++ Binge/src/pages/Landing.jsx
@@ -13,7 +13,7 @@ const Landing = () => {
           <div className="menu">
             <h1>Binge: Din personliga kock med AI-skapade recept</h1>
             <p>tforska en värld av smak och kreativitet med hjälp av avancerad AI-teknik. Binge ger dig skräddarsydda recept som matchar dina smakpreferenser och livsstil</p>
-            <button>Börja dinmatresa</button>
+            <button>Börja din matresa</button>
           </div>
           <div className="card">
           
--- Binge/src/pages/SignIn.jsx
+++ Binge/src/pages/SignIn.jsx
@@ -1,128 +1,99 @@
-
 import { useForm } from "react-hook-form";
 import Navbar from "../components/Navbar";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
+const SignInPage = () => {
+  const navigate = useNavigate();
 
+ const handleSubmit = (data) => {
+   const userData = JSON.parse(localStorage.getItem(data.email));
+   console.log(data.email)
+   console.log(userData)
+   if (userData) {
+     if (userData.password === data.password) {
+       console.log(userData.firstName + " You Are Successfully Logged In");
+       navigate("/profile");
+     } else {
+       console.log("Password is incorrect");
+     }
+   } else {
+     console.log("User with provided email does not exist");
+   }
+ };
 
-
-const signInPage= () => {
   return (
     <>
       <Navbar />
       <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
         <section className="size-[40rem] border-solid border-black border-8 flex flex-col justify-center items-center bg-white p-8 rounded-lg">
-          <div className="flex flex-col items-center gap-4">
-            <h1 className="text-xl font-bold">Registrera Dig</h1>
-            <button
-              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 my-5"
-              type="submit"
-            >
-              Sign In With Google
-            </button>
-          </div>
-          <SignInForm />
+         
+          <SignInForm onSubmit={handleSubmit} />
         </section>
       </div>
     </>
   );
-}
+};
 
-const SignInForm = () => {
+const SignInForm = ({ onSubmit }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-    // Handle form submission here
-  };
-
   return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="max-w-md mx-auto grid grid-cols-2 gap-4"
-    >
-      <div className="mb-4">
-        <label htmlFor="firstName" className="block mb-1">
-          First Name
-        </label>
-        <input
-          className="border-solid border-black border-2 py-2 px-4 w-full"
-          id="firstName"
-          {...register("firstName", { required: true })}
-        />
-        {errors.firstName && (
-          <span className="text-red-500">This field is required</span>
-        )}
-      </div>
-      <div className="mb-4">
-        <label htmlFor="lastName" className="block mb-1">
-          Last Name
-        </label>
-        <input
-          className="border-solid border-black border-2 py-2 px-4 w-full"
-          id="lastName"
-          {...register("lastName", { required: true })}
-        />
-        {errors.lastName && (
-          <span className="text-red-500">This field is required</span>
-        )}
-      </div>
-      <div className="mb-4">
-        <label htmlFor="username" className="block mb-1">
-          Username
-        </label>
-        <input
-          className="border-solid border-black border-2 py-2 px-4 w-full"
-          id="username"
-          {...register("username", { required: true })}
-        />
-        {errors.username && (
-          <span className="text-red-500">This field is required</span>
-        )}
-      </div>
-      <div className="mb-4">
-        <label htmlFor="email" className="block mb-1">
-          Email
-        </label>
-        <input
-          className="border-solid border-black border-2 py-2 px-4 w-full"
-          id="email"
-          type="email"
-          {...register("email", { required: true })}
-        />
-        {errors.email && (
-          <span className="text-red-500">This field is required</span>
-        )}
-      </div>
-      <div className="mb-4">
-        <label htmlFor="password" className="block mb-1">
-          Password
-        </label>
-        <input
-          className="border-solid border-black border-2 py-2 px-4 w-full"
-          id="password"
-          type="password"
-          {...register("password", { required: true })}
-        />
-        {errors.password && (
-          <span className="text-red-500">This field is required</span>
-        )}
-      </div>
-      <button
-        className="bg-blue-500 text-white py-2 px-4 col-span-2 rounded hover:bg-blue-600"
-        type="submit"
-      >
-        Sign In
-      </button>
+    <>
+      <div className="bg-gray-200 min-h-screen flex flex-col justify-center items-center">
+        <section className="size-[40rem] border-solid border-black border-8 flex flex-col justify-center items-center bg-white p-8 rounded-lg">
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="text-xl font-bold">Logga in</h1>
+            <button
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 my-5"
+              type="submit"
+            >
+              Logga In Med Google
+            </button>
+          </div>
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="max-w-md mx-auto grid grid-cols-1 gap-4"
+          >
+            <div className="mb-4">
+              <label className="block mb-1" htmlFor="usernameOrEmail">
+                Email
+              </label>
+              <input
+                className="border-solid border-black border-2 py-2 px-4 w-full"
+                id="Email"
+                {...register("email", { required: true })}
+              />
+              {errors.Email && <span>This field is required</span>}
+            </div>
+            <div className="mb-4">
+              <label className="block mb-1" htmlFor="password">
+                Password
+              </label>
+              <input
+                className="border-solid border-black border-2 py-2 px-4 w-full"
+                id="password"
+                type="password"
+                {...register("password", { required: true })}
+              />
+              {errors.password && <span>This field is required</span>}
+            </div>
+            <button
+              className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 my-5"
+              type="submit"
+            >
+              Log In
+            </button>
 
-      <Link to="/signup">Already got an account?</Link>
-    </form>
+            <Link to="/signup"> Dont have an Account?</Link>
+          </form>
+        </section>
+      </div>
+    </>
   );
 };
 
-
-export default signInPage;
+export default SignInPage;
