1d9752b2002ea5b77ad827a3e3bd1582c35e7b32
Lilikh <lilikheirandish051@gmail.com>
2024-05-02 15:13:03 +0200

    add 10 dishes for fakedataJson

--- Binge/src/fakeData.jsx
+++ Binge/src/fakeData.jsx
@@ -93,6 +93,77 @@ const fakeData=[
     "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
     "Garnera med basilikablad och servera med ris, potatis eller pasta."],
 },
+  {  
+    
+    id:7,
+    title:"Thainudlar med kycklingfärssås",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1541664398/353505.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    
+    id:8,
+    title:"Kycklingköttbullar med snabb sås och rårörda lingon",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1518419790/278609.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    
+    id:6,
+    title:"Indisk spenatgryta med pocherat ägg",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1536566399/349255.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    
+    id:9,
+    title:"Salade niçoise med grillad tonfisk",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1432635820/32295.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+
+  {  
+    
+    id:10,
+    title:"Kycklingköttbullar i tomatsås",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1669897031/cloud/268102.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
 
 
 
