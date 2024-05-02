7ab4dc5b0f7adf55a9606260a3f5f567369718c2
Lilikh <lilikheirandish051@gmail.com>
2024-05-02 15:06:05 +0200

    add DataFakeJason

---
+++ Binge/src/fakeData.jsx
@@ -0,0 +1,106 @@
+const fakeData=[
+  { 
+    id:0,
+    title:" Kycklinggratäng",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1640089222/cloud/241883.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  { 
+    id:1,
+    title:" Grekisk pyttipanna",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1669903607/cloud/268178.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    id:2,
+    title:" Falukorvskebab",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1676451610/cloud/273908.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    id:3,
+    title:" Pasta med svampsås",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1432634025/30465.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    
+    id:4,
+    title:" Paprika- och kikärtsburgare med dillslungad gurksallad",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1492441100/196882.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    
+    id:5,
+    title:" Chèvrepiroger",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1432634136/30569.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+  {  
+    
+    id:6,
+    title:"Indisk spenatgryta med pocherat ägg",
+    imgUrl:"https://res.cloudinary.com/coopsverige/image/upload/f_auto,fl_progressive,q_90,g_center,h_800,w_800/v1536566399/349255.jpg",
+    Ingredienser:["600 g kycklingfilé","salt och svartpeppar",
+    "smör, till stekning","2 dl crème fraiche","1,5 dl grädde1 vitlöksklyfta","riven","1 dl soltorkade tomater", "strimlade 1 msk kycklingfond",
+    "0,5 tsk salt","0,5 tsk grovmalen svartpeppar","2 dl riven ost"
+    ],
+    description:["Sätt ugnen på 200°. Salta och peppra kycklingfiléerna runtom. Stek dem i smör ca 1 min per sida.",
+    "Lägg i en ugnsform. Rör ihop crème fraiche, grädde, vitlök, tomater, fond, salt och peppar.",
+    "Häll såsen i formen och strö över ost. Gratinera mitt i ugnen tills kycklingen är genomstekt och gratängen har fin färg, ca 25 min.",
+    "Garnera med basilikablad och servera med ris, potatis eller pasta."],
+},
+
+
+
+
+]
+
+
+
+
+
+export default fakeData;
