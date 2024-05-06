04e2f4adaa64075a0b7871ee93b7b421b7c8a7ad
Lilikh <lilikheirandish051@gmail.com>
2024-05-06 19:56:15 +0200

    chang bg-color navbar

--- Binge/src/components/Navbar.jsx
+++ Binge/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import logo from "../components/BINGE.png"
 const Navbar = () => {
   return (
   
-<nav className="bg-white border-gray-200 dark:bg-gray-200">
+<nav className="bg-white border-gray-200 dark:bg-gray-500">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 <Link to="/">
 <a href="#" className="flex items-center space-x-3 rtl:space-x-reverse">
