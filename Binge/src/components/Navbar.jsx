214f67f63393a39c4b04ed2ee41a3b393ffa4de0
Carl <carl.mikaelsson@chasacademy.se>
2024-05-22 18:43:20 +0200

    added: correct page links, ux, clickable "images"

--- Binge/src/components/Navbar.jsx
+++ Binge/src/components/Navbar.jsx
@@ -53,7 +53,7 @@ const Navbar = () => {
           <a href="#" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500" aria-current="page">Home</a>
         </li>
       </Link> */}
-      <Link to="./Cookbook">
+      <Link to="../Cookbook">
       <li>
         <a href="#" className="block ml-5  py-2 px-3 text-gray-900 rounded hover:bg-gray-200 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray md:dark:hover:text-blue-500 dark:hover:bg-gray-800 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Min Kokbok</a>
       </li>
--- Binge/src/pages/Cookbook.jsx
+++ Binge/src/pages/Cookbook.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
-import Popup from '../components/ClickEnlargeFood';
+import Popup from '../components/ClickEnlargeFood'
+import Footer from "../components/Footer"
+import { Link } from "react-router-dom"
 import '/src/styles/Cookbook.css'
 
 function Cookbook() {
@@ -19,25 +21,36 @@ function Cookbook() {
       <Navbar />
       { /* Copy+paste the code below as a component with the name "Cookbook" */}
       <div className="flex flex-col p-5 bg-gray-300 border-b border-solid border-black">
-        <div className="flex bg-white border border-solid border-black dark:bg-gray-900 dark:text-white">
+        {/* <div className="flex bg-white border border-solid border-black dark:bg-gray-900 dark:text-white">
           <p>Binge Searchbar</p>
-        </div>
-        <div className="flex">
-          {/* <div className="bg-white p-5 my-10 border border-solid border-black">
-              <p>Smakprofil</p>
-              <p>Svepa</p>
-              <p>Kokbok</p>
-            </div> */}
-          <div className="flex-1 my-10">
-            <h1 className="flex justify-around">
-              Din kokbok - Sparade recept
-            </h1>
-            <div className="flex items-center justify-center">
-              <div className="mr-5 p-5 bg-white border border-solid border-black">
-                Bild på rätt
+        </div> */}
+        <h1 className="flex rt-r-weight-bold justify-center">
+          Din kokbok - Gillade recept
+        </h1>
+        <p className='flex justify-center'>
+          Klicka på korten för att få en mer detaljerad vy på ditt AI-genererade recept!
+        </p>
+        <div className="flex my-2">
+          {/* Add clickable links to three paragraphs directly below */}
+          <div className="bg-white p-5 ml-80 mr-60 border border-solid border-black" style={{ borderRadius: '10%' }}>
+            <Link to='../Profile'>
+              <p className='hover:bg-gray-20 md:hover:text-blue-700'>Smakprofil</p>
+            </Link>
+            <Link to='../Swipe'>
+              <p className='hover:bg-gray-20 md:hover:text-blue-700'>Svepa</p>
+            </Link>
+            <Link to="../Cookbook">
+              <p className='hover:bg-gray-20 md:hover:text-blue-700'>Kokbok</p>
+            </Link>
+          </div>
+          <div className="flex-1" style={{ maxWidth: '30%' }}>
+            <div className="flex justify-evenly">
+              {/* Swiping right on the Swipe page -> Generate divs akin to below. */}
+              <div className="p-5 bg-white border border-solid border-black hover:bg-gray-20 md:hover:text-blue-700" style={{ maxWidth: '250px', borderRadius: '5%' }} onClick={seeFood}>
+                {/* Bild på rätt */}
               </div>
-              <div className=" p-5 bg-white border border-solid border-black">
-                Bild på rätt
+              <div className="p-5 bg-white border border-solid border-black hover:bg-gray-20 md:hover:text-blue-700" style={{ maxWidth: '250px', borderRadius: '5%' }} onClick={seeFood}>
+                {/* Bild på rätt */}
               </div>
             </div>
           </div>
@@ -45,14 +58,12 @@ function Cookbook() {
       </div>
 
       {/* Popup window for liked food. 
-      To add in future: connect with specific clicked saved food. */}
-      <div className="App">
-        <button className="mt-4 px-4 py-2 bg-green-500 text-white rounded" onClick={seeFood}>
-          See food here!
-        </button>
+      To add in future: connect with specific clicked saved food as a general function.*/}
+      <div className="myChosenFood">
         {isFoodOpen && <Popup onClose={noFood} />}
       </div>
       {/* <Popup /> */}
+      <Footer />
     </>
   )
 }
