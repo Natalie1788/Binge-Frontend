6ee06469397796e5fca573c7191bba11083c91b2
Carl <carl.mikaelsson@chasacademy.se>
2024-05-22 20:25:29 +0200

    added: centered content independent of links section

--- Binge/src/pages/Cookbook.jsx
+++ Binge/src/pages/Cookbook.jsx
@@ -19,38 +19,35 @@ function Cookbook() {
   return (
     <>
       <Navbar />
-      { /* Copy+paste the code below as a component with the name "Cookbook" */}
-      <div className="flex flex-col p-5 bg-gray-300 border-b border-solid border-black">
-        {/* <div className="flex bg-white border border-solid border-black dark:bg-gray-900 dark:text-white">
-          <p>Binge Searchbar</p>
-        </div> */}
-        <h1 className="flex rt-r-weight-bold justify-center">
-          Din kokbok - Gillade recept
-        </h1>
-        <p className='flex justify-center'>
-          Klicka på korten för att få en mer detaljerad vy på ditt AI-genererade recept!
-        </p>
-        <div className="flex my-2">
-          {/* Add clickable links to three paragraphs directly below */}
-          <div className="bg-white p-5 ml-80 mr-60 border border-solid border-black" style={{ borderRadius: '10%' }}>
-            <Link to='../Profile'>
-              <p className='hover:bg-gray-20 md:hover:text-blue-700'>Smakprofil</p>
-            </Link>
-            <Link to='../Swipe'>
-              <p className='hover:bg-gray-20 md:hover:text-blue-700'>Svepa</p>
-            </Link>
-            <Link to="../Cookbook">
-              <p className='hover:bg-gray-20 md:hover:text-blue-700'>Kokbok</p>
-            </Link>
-          </div>
-          <div className="flex-1" style={{ maxWidth: '30%' }}>
-            <div className="flex justify-evenly">
-              {/* Swiping right on the Swipe page -> Generate divs akin to below. */}
-              <div className="p-5 bg-white border border-solid border-black hover:bg-gray-20 md:hover:text-blue-700" style={{ maxWidth: '250px', borderRadius: '5%' }} onClick={seeFood}>
-                {/* Bild på rätt */}
-              </div>
-              <div className="p-5 bg-white border border-solid border-black hover:bg-gray-20 md:hover:text-blue-700" style={{ maxWidth: '250px', borderRadius: '5%' }} onClick={seeFood}>
-                {/* Bild på rätt */}
+      <div className="relative bg-gray-300 border-b border-solid border-black p-5" style={{ display: 'grid', gridTemplateColumns: 'auto 1fr' }}>
+        <div className="p-5 bg-white border border-solid border-black" style={{ borderRadius: '10%', minWidth: '10%', maxWidth:'100%'}}>
+          <Link to='../Profile'>
+            <p className='hover:bg-gray-20 md:hover:text-blue-700'>Smakprofil </p>
+          </Link>
+          <Link to='../Swipe'>
+            <p className='hover:bg-gray-20 md:hover:text-blue-700'>Svepa</p>
+          </Link>
+          <Link to="../Cookbook">
+            <p className='hover:bg-gray-20 md:hover:text-blue-700'>Kokbok</p>
+          </Link>
+        </div>
+        <div className="absolute left-1/2 transform -translate-x-1/2 w-full">
+          <div className="flex flex-col justify-center items-center w-full">
+            <h1 className="flex justify-around items-center rt-r-weight-bold">
+              Din kokbok - Gillade recept
+            </h1>
+            <p className='flex justify-around'>
+              Klicka på korten för att få en mer detaljerad vy på ditt AI-genererade recept!
+            </p>
+            <div className="justify-center items-center w-full" style={{ maxWidth: '10%', margin: '0 auto' }}>
+              <div className="flex justify-around w-full">
+                {/* Swiping right on the Swipe page -> Generate divs akin to below. */}
+                <div className="p-5 bg-white border border-solid border-black hover:bg-gray-20 md:hover:text-blue-700" style={{ maxWidth: '100%', borderRadius: '5%' }} onClick={seeFood}>
+                  {/* Bild på rätt */}
+                </div>
+                <div className="p-5 bg-white border border-solid border-black hover:bg-gray-20 md:hover:text-blue-700" style={{ maxWidth: '100%', borderRadius: '5%' }} onClick={seeFood}>
+                  {/* Bild på rätt */}
+                </div>
               </div>
             </div>
           </div>
