41e16cbf3f6ec975b453ed68032f2a3e68e0e8c3
GaKa00 <145324695+GaKa00@users.noreply.github.com>
2024-06-03 17:34:03 +0200

    resolved mobilenav targetability

--- Binge/src/components/mobileNav.jsx
+++ Binge/src/components/mobileNav.jsx
@@ -1,32 +1,27 @@
-import { FaUser } from "react-icons/fa";
-import { FaArrowsAltH } from "react-icons/fa";
-import { FaBook } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { FaUser, FaArrowsAltH, FaBook } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
 export const MobileNav = () => {
-
-return(
-
-
-<nav className="bg-white p-5  border-t border-solid border-black justify-around w-full absolute bottom-0 sm:flex md:hidden z-50">
-  <div className="flex flex-col items-center">
-    <Link to="/profile">
-      <FaUser size={30} className="ml-6" />
-      <h4 className="text-center"> Smakprofil</h4>
-    </Link>
-  </div>
-  <div className="flex flex-col items-center">
-    <Link to="/swipe">
-      <FaArrowsAltH size={30} className="ml-2" />
-      <h4 className="text-center"> Svepa</h4>
-    </Link>
-  </div>
-  <div className="flex flex-col items-center">
-    <Link to="/cookbook">
-      <FaBook size={30} className="ml-2" />
-      <h4 className="text-center">Kokbok</h4>
-    </Link>
-  </div>
-</nav>
-)
+  return (
+    <nav className="bg-white p-5 border-t border-solid border-black justify-around w-full absolute bottom-0 sm:flex md:hidden z-50">
+      <div className="flex flex-col items-center">
+        <NavLink to="/profile" className={({ isActive }) => isActive ? 'underline font-bold' : ''}>
+          <FaUser size={30} className="ml-2" />
+          <h4 className="text-center">Profile</h4>
+        </NavLink>
+      </div>
+      <div className="flex flex-col items-center">
+        <NavLink to="/swipe" className={({ isActive }) => isActive ? 'underline font-bold' : ''}>
+          <FaArrowsAltH size={30} className="ml-1" />
+          <h4 className="text-center">Swipe</h4>
+        </NavLink>
+      </div>
+      <div className="flex flex-col items-center">
+        <NavLink to="/cookbook" className={({ isActive }) => isActive ? 'underline font-bold' : ''}>
+          <FaBook size={30} className="ml-5" />
+          <h4 className="text-center">Cookbook</h4>
+        </NavLink>
+      </div>
+    </nav>
+  );
 }
